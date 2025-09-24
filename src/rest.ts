fa0e586fb10bd8f84e8d8d72a40b45cce826d69c
jorinho <jorinho@jota.one>
2025-09-24 19:50:13 +0200

    feat: return a ky instance in rest function

--- src/rest.ts
+++ src/rest.ts
@@ -1,11 +1,4 @@
-import ky from 'ky';
-export default function rest() {
-    return {
-        get: ky.get,
-        post: ky.post,
-        put: ky.put,
-        delete: ky.delete,
-        patch: ky.patch,
-        head: ky.head,
-    }
+import ky, { type Options } from "ky";
+export default function rest(config: Options = {}) {
+  return ky.create(config);
 }
