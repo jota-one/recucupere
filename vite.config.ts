980fb5baef29c8bcfdf38dc886a42e009324c608
jorinho <jorinho@jota.one>
2025-09-24 16:16:11 +0200

    chore: add types exports in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  // config options
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
